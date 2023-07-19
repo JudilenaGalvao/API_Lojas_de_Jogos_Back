b0bec5cc725bfc16891634f26ad91bca9b84b14a
Judilena Galvão <127363645+JudilenaGalvao@users.noreply.github.com>
2023-07-19 08:32:41 -0300

    Delete jogos directory

--- jogos/src/main/java/com/example/jogos/repository/CompraRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.jogos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.jogos.domain.Compra;
-
-public interface CompraRepository extends IGenericRepository<Compra>{
-    
-}
--- jogos/src/main/java/com/example/jogos/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jogos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.jogos.domain.Endereco;
-
-public interface EnderecoRepository extends IGenericRepository<Endereco>{
-    
-}
